935520ae608a954b0a22d36f5844e14dd1ad574f
Jacob Sin <jacob.sin@gmail.com>
2016-09-18 18:21:34 +0800

    added some functional tests of different style

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
-import play.libs.F.Callback;
-import play.test.TestBrowser;
+import play.mvc.Result;
+import play.test.FakeRequest;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
@@ -12,10 +12,22 @@ public class IntegrationTest {
      * in this example we just check if the welcome page is being shown
      */
     @Test
-    public void test() {
+    public void browserTest() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
             browser.goTo("http://localhost:3333");
             assertThat(browser.pageSource()).contains("Your new application is ready.");
+        });
+    }
+
+    @Test
+    public void httpTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), () -> {
+            FakeRequest request = fakeRequest("GET", "/freemarker");
+            Result result = route(request);
+            assertThat(contentAsString(result)).isEqualTo("<table>\n" +
+                    "    <thead><tr><th>user</th><th>product</th></tr></thead>\n" +
+                    "    <tbody><tr><td>user</td><td>product</td></tr></tbody>\n" +
+                    "</table>");
         });
     }
 
